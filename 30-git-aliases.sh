d1d24972458843fd139a91a6bfebdb4f86022713
Barney Desmond <barney.desmond@det.nsw.edu.au>
2015-08-27 10:58:07 +1000

    Dedupe the cf alias

--- 30-git-aliases.sh
+++ 30-git-aliases.sh
@@ -1,3 +1,3 @@
-alias cf='cd ~/svn/cfinputs ; svn up'
+alias cf='cd ~/git/cfengine/trunk ; svn up'
 alias pup='cd ~/git/puppet ; make -C ~/git/puppet pull'
 alias nag='cd ~/git/nagios-puttputt ; git pull'
