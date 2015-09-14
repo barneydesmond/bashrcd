1159a1e7e10795f132541d86d0113b147116a2b7
Barney Desmond <barneydesmond@gmail.com>
2015-09-14 16:16:13 +1000

    how did I not have proxies before

---
+++ 10-proxies.sh
@@ -0,0 +1,6 @@
+if ip link list | grep -q '00:0c:29:01:cb:a5' ; then
+	echo -n "DET workstation detected, enabling proxies, "
+	export http_proxy=proxy.det.nsw.edu.au:80
+	export https_proxy=proxy.det.nsw.edu.au:80
+	export ftp_proxy=proxy.det.nsw.edu.au:80
+fi
