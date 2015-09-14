0f53b1b2ec8f5f997189cbde65f85fd7ff7d0135
Barney Desmond <barneydesmond@gmail.com>
2015-09-14 16:16:03 +1000

    new puppet repo path

--- 30-git-aliases.sh
+++ 30-git-aliases.sh
@@ -1,3 +1,3 @@
 alias cf='cd ~/git/cfengine/trunk ; svn up'
-alias pup='cd ~/git/puppet ; make -C ~/git/puppet pull'
+alias pup='cd ~/git/dec-puppet ; make -C ~/git/dec-puppet pull'
 alias nag='cd ~/git/nagios-puttputt ; git pull'
