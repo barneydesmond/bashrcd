5f46915c69572b28f83a90d86e71e2bc4ab4cc54
Barney Desmond <barneydesmond@gmail.com>
2015-11-20 22:29:47 +1000

    Remove the rest of the old cruft

--- 30-misc-aliases.sh
+++ 30-misc-aliases.sh
@@ -16,14 +16,7 @@ alias pws='openssl rand -base64 16 | tr -cd a-hjkp-zA-HJKP-Z2-9 | cut -c 1-10'
 
 alias go='gnome-open'
 
-alias wgetapi='wget --user="$API_AUTH_USER" --password="$API_AUTH_PASS" -S'
-
 alias fs='find . -path "*/.git" -prune -o -type f -print'
 alias fsg='find . -type f'
 alias ds='find . -path "*/.git" -prune -o -type d -print'
 alias dsg='find . -type d'
-
-# Uses the virtualenvwrapper
-alias ve='workon everything'
-
-alias cleanmarquise='cd ~/git/ && rm -rf libmarquise && git clone barneydesmond.github.com:anchor/libmarquise.git && cd libmarquise/ && echo -e "Now run:\n    autoreconf -if\n    ./configure"'
