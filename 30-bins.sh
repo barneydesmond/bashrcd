0f3d5fc392d180743963a5bd056e7ea88ebdae21
Barney Desmond <barneydesmond@gmail.com>
2017-03-22 11:30:27 +1100

    Fix up PATH for extra bin dirs

--- 30-bins.sh
+++ 30-bins.sh
@@ -1,4 +1,11 @@
 if [ -d ~/git/dec-tools/bin ] ; then
 	export PATH=~/git/dec-tools/bin:$PATH
+fi
+
+if [ -d ~/bin ] ; then
 	export PATH=~/bin:$PATH
+fi
+
+if [ -d ~/.local/bin ] ; then
+	export PATH=~/.local/bin:$PATH
 fi
