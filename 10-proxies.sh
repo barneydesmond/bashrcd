7382f15b044df251333eb68c3091f431c33cc026
Barney Desmond <barneydesmond@gmail.com>
2017-06-05 12:24:42 +1000

    New workstation

--- 10-proxies.sh
+++ 10-proxies.sh
@@ -1,4 +1,5 @@
-if /sbin/ip link list | grep -q '00:0c:29:01:cb:a5' ; then
+#!/bin/bash
+if /sbin/ip link list | egrep -q 'ether (00:50:56:22:33:44|00:0c:29:01:cb:a5) ' ; then
 	echo -n "DET workstation detected, enabling proxies, "
 	export http_proxy=proxy.det.nsw.edu.au:80
 	export https_proxy=proxy.det.nsw.edu.au:80
