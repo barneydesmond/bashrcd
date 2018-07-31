d378aa565cea49488bb7f5f6ff59347359b216ce
Barney Desmond <barneydesmond@gmail.com>
2018-07-31 18:11:09 +1000

    Load git-bump-branch completions for shell

--- 30-git-aliases.sh
+++ 30-git-aliases.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
 alias cf='cd ~/git/cfengine/trunk ; svn up'
 alias pup='cd ~/git/dec-puppet ; make -C ~/git/dec-puppet pull'
+
+if [ -f ~/git/dec-tools/bin/bump_branch_completions ] ; then
+	. ~/git/dec-tools/bin/bump_branch_completions
+fi
 
 alias kw='svn ps svn:keywords "Id URL"'
