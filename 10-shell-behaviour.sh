e0624c21057dae28c0285f44a9354ef0986eca18
Barney Desmond <barneydesmond@gmail.com>
2018-08-13 11:28:49 +1000

    Load bash completions
    I have no idea how I missed these before...

--- 10-shell-behaviour.sh
+++ 10-shell-behaviour.sh
@@ -1,3 +1,8 @@
 # Check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
+
+# Load tab completions, this path is for Fedora
+if [ -f /etc/profile.d/bash_completion.sh ] ; then
+	. /etc/profile.d/bash_completion.sh
+fi
