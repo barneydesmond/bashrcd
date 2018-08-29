2dd88806c9ea5e6907cca582cc26d8c0e04d6f95
Barney Desmond <barneydesmond@gmail.com>
2018-08-29 13:40:27 +1000

    Tab-complete for vault

--- 30-bins.sh
+++ 30-bins.sh
@@ -9,3 +9,8 @@ fi
 if [ -d ~/.local/bin ] ; then
 	export PATH=~/.local/bin:$PATH
 fi
+
+# Tab-complete for vault
+if hash vault 2>/dev/null ; then
+	complete -C /home/bdesmond1/.local/bin/vault vault
+fi
