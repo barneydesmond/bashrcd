e80b767bdf3822bf0aed5db1da4987347c7b3924
Barney Desmond <barneydesmond@gmail.com>
2018-09-05 15:19:45 +1000

    Add alias to connect to Cirrus jumphost

--- 30-det-aliases.sh
+++ 30-det-aliases.sh
@@ -2,3 +2,5 @@ alias pnow='sudo /opt/puppetlabs/bin/puppet agent --test'
 
 # Run directly from linux workstation
 alias linuxkeepassx='ssh -X pu0991dira0001.svcmgt.mgmt.det /home/network/jroberts337/bin/open_keepassx'
+
+alias cirrus='xfreerdp +clipboard /u:SAbdesmond1 /p:"${SApassword}" '"/size:${CIRRUS_RDP_WIDTH:-1024}x${CIRRUS_RDP_HEIGHT:-768} /g:mfardg.det.nsw.edu.au /v:pw0991jhst0001.nsw.education /drive:vectorhome,${HOME}"
