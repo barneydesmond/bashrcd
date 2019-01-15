f067f3e868e68bd9ae708f8bcd9c3c67959d46c1
Barney Desmond <barneydesmond@gmail.com>
2019-01-15 17:18:01 +1100

    Disable bash history expansion

--- 10-history.sh
+++ 10-history.sh
@@ -1,2 +1,5 @@
 # Timestamps in shell history
 export HISTTIMEFORMAT='%Y-%m-%d %H:%M:%S  '
+
+# Disable the use of ! for history expansion (so you can echo bangs in strings easily)
+set +o histexpand
