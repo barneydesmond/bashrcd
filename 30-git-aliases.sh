d962a2defc378cbe7f3176207711dbb689b29646
Barney Desmond <barneyd@arista.com>
2019-06-14 12:21:18 +1000

    Cleanup more old aliases

--- 30-git-aliases.sh
+++ 30-git-aliases.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-alias cf='cd ~/git/cfengine/trunk ; svn up'
-alias pup='cd ~/git/dec-puppet ; make -C ~/git/dec-puppet pull'
-
-if [ -f ~/git/dec-tools/bin/bump_branch_completions ] ; then
-	. ~/git/dec-tools/bin/bump_branch_completions
-fi
-
-if [ -f ~/git/dec-tools/bin/netgroup_completions ] ; then
-	. ~/git/dec-tools/bin/netgroup_completions
-fi
-
 alias kw='svn ps svn:keywords "Id URL"'
 
 alias bfg='java -jar /usr/share/bfg/bfg-1.13.0.jar'
