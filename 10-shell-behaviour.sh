2c36beb0bd8675d0148406db8dd3e010475e34b0
Barney Desmond <barneyd@arista.com>
2020-02-27 13:43:06 +1100

    Fix OSX bash completions

--- 10-shell-behaviour.sh
+++ 10-shell-behaviour.sh
@@ -7,7 +7,11 @@ if [ -f /etc/profile.d/bash_completion.sh ] ; then
 	. /etc/profile.d/bash_completion.sh
 fi
 
-# This is OSX, with bash-completion installed via brew
+# This is OSX, with bash-completion installed via brew. You need a newer
+# version of bash than the OS default (which is 3.2), and completions to match.
+# https://kubernetes.io/docs/tasks/tools/install-kubectl/#enabling-shell-autocompletion
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
-
 [[ -r "/Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash" ]] && . "/Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash"
+
+#If you'd like to use existing homebrew v1 completions, add the following before the first sourcing line:
+#export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
