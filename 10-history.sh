6f8098f9bd6a7657c331d4f89e6eff8310364565
Barney Desmond <barneyd@arista.com>
2021-03-16 15:55:20 +1100

    Copy some bash history controls from stock .bashrc

--- 10-history.sh
+++ 10-history.sh
@@ -1,5 +1,16 @@
 # Timestamps in shell history
 export HISTTIMEFORMAT='%Y-%m-%d %H:%M:%S  '
 
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=10000
+HISTFILESIZE=20000
+
 # Disable the use of ! for history expansion (so you can echo bangs in strings easily)
 set +o histexpand
