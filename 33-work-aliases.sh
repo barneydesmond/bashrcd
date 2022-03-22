e021c7662d835d9634e8d6ded808519573f91ae1
Barney Desmond <barneyd@arista.com>
2022-03-22 17:15:39 +1100

    Add pastebin alias for work

--- 33-work-aliases.sh
+++ 33-work-aliases.sh
@@ -7,3 +7,7 @@ alias squeue='squeue -o "%9i %35j %12P %4C %7m %9u %11T %9M %16R %10p %E"'
 if hash py.test-3 2>/dev/null ; then
 	hash py.test 2>/dev/null || alias py.test='py.test-3'
 fi
+
+# Internal pastebin http://pb/
+# /usr/bin/some_cmd | pb
+alias pb='curl -F c=@- pb'
