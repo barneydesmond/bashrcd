0456f506e0f0dda65e7a750247f9ff37913d4dc2
Barney Desmond <barneyd@arista.com>
2022-07-05 18:33:17 +1000

    Add 'dns' alias for jumping to etc-hosts in repo

--- 33-work-aliases.sh
+++ 33-work-aliases.sh
@@ -1,4 +1,5 @@
 alias mem='cd ~/git/mm/projects/sysadmin/dutty/ ; git pull'
+alias dns='cd ~/git/mm/projects/sysadmin/ansible/infra/dnsmasq ; git pull'
 
 # Slurm tools
 alias squeue='squeue -o "%9i %35j %12P %4C %7m %9u %11T %9M %16R %10p %E"'
