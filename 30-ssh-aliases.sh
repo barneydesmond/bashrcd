6bddae14ff5df3051c05ab806b77a9ee4fd192ea
Barney Desmond <barneyd@arista.com>
2022-08-15 09:30:38 +1000

    Add handy ssh-after-reboot alias

--- 30-ssh-aliases.sh
+++ 30-ssh-aliases.sh
@@ -2,4 +2,7 @@ alias sho='ssh -o StrictHostKeyChecking=no'
 alias shop='ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=yes -o PubkeyAuthentication=no'
 alias sco='scp -o StrictHostKeyChecking=no'
 
+# Very useful after rebooting a server and you're waiting to get back on it
+alias sshreboot='ssh -o ConnectTimeout=1 -o ConnectionAttempts=999'
+
 alias keymake='make -C ~/.ssh/'
