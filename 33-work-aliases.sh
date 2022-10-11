ebcd537d8d23ece1c7989a1a910be7f5a3808ba1
Barney Desmond <barneyd@arista.com>
2022-10-11 16:17:34 +1100

    More shell jumps into the repo

--- 33-work-aliases.sh
+++ 33-work-aliases.sh
@@ -1,5 +1,7 @@
 alias mem='cd ~/git/mm/projects/sysadmin/dutty/ ; git pull'
 alias dns='cd ~/git/mm/projects/sysadmin/ansible/infra/dnsmasq ; git pull'
+alias ans='cd ~/git/infra/ansible/'
+alias ks='cd ~/git/infra/k8s/'
 
 # Slurm tools
 alias squeue='squeue -o "%9i %35j %12P %4C %7m %9u %11T %9M %16R %10p %E"'
