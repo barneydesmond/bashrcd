4c3c43f9147aec54ffa0c3201a62d0fc7cafb6d2
Barney Desmond <barneyd@arista.com>
2023-08-09 09:30:53 +1000

    Add alias for cleaning a host out of known_hosts

--- 30-ssh-aliases.sh
+++ 30-ssh-aliases.sh
@@ -6,3 +6,5 @@ alias sco='scp -o StrictHostKeyChecking=no'
 alias sshreboot='ssh -o ConnectTimeout=1 -o ConnectionAttempts=999'
 
 alias keymake='make -C ~/.ssh/'
+
+alias clean_known_hosts='ssh-keygen -f ~/.ssh/known_hosts -R'
